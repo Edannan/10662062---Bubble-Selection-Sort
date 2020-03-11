cbf277ff9179126500d7a3c51a85119f34760fb1
Edannan <42790712+Edannan@users.noreply.github.com>
2020-03-11 11:39:46 +0000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,39 +4,29 @@ using namespace std;
 
 int main()
 {
-    int i,j,n,loc,temp,min,a[30];
-    cout<<"Enter the number of elements:";
-    cin>>n;
-    cout<<"\nEnter the elements\n";
+	int a[50],n,i,j,temp;
+	cout<<"Enter the size of array: ";
+	cin>>n;
+	cout<<"Enter the array elements: ";
 
-    for(i=0;i<n;i++)
-    {
-        cin>>a[i];
-    }
+	for(i=0;i<n;++i)
+		cin>>a[i];
 
-    for(i=0;i<n-1;i++)
-    {
-        min=a[i];
-        loc=i;
-        for(j=i+1;j<n;j++)
-        {
-            if(min>a[j])
-            {
-                min=a[j];
-                loc=j;
-            }
-        }
+	for(i=1;i<n;++i)
+	{
+		for(j=0;j<(n-i);++j)
+			if(a[j]>a[j+1])
+			{
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+	}
 
-        temp=a[i];
-        a[i]=a[loc];
-        a[loc]=temp;
-    }
+	cout<<"Array after bubble sort:";
 
-    cout<<"\nSorted list is as follows\n";
-    for(i=0;i<n;i++)
-    {
-        cout<<a[i]<<" ";
-    }
+	for(i=0;i<n;++i)
+		cout<<" "<<a[i];
 
-    return 0;
+	return 0;
 }
